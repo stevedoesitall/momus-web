1ae07945ddec4a1e5349d8a8681ef2161878d42a
Steve Giordano <stephenagiordano@gmail.com>
2021-06-18 17:32:55 -0400

    Latest updates

--- src/pages/Deity.svelte
+++ src/pages/Deity.svelte
@@ -18,7 +18,7 @@
 	<h1 class="is-family-sans-serif is-lowercase has-text-weight-bold grey-dark">
 		{deity.name}
 	</h1>
-	<h2>A {deity.region}</h2>
+	<h2>A {deity.region} {deity.type}</h2>
 	<h2>Domains:</h2>
 	{#each domain as d (d)}
 		<p>{d}</p>
