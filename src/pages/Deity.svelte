707d2294c6a0ee19dc2ee9c5c7389db37aa6a7a6
Steve Giordano <stephenagiordano@gmail.com>
2021-06-18 14:01:06 -0400

    Fixed styling, broke APIs into separate file

--- src/pages/Deity.svelte
+++ src/pages/Deity.svelte
@@ -1,5 +1,4 @@
 <script>
-	//Should move these to separate file
 	export let params
 
 	let deity = {}
@@ -19,7 +18,7 @@
 	<h1 class="is-family-sans-serif is-lowercase has-text-weight-bold grey-dark">
 		{deity.name}
 	</h1>
-	<h2>A {deity.region} {deity.type}</h2>
+	<h2>A {deity.region}</h2>
 	<h2>Domains:</h2>
 	{#each domain as d (d)}
 		<p>{d}</p>
