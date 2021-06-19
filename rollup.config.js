83e76aac159e63edc169e6933410226e4dcd9142
Steve Giordano <stephenagiordano@gmail.com>
2021-06-19 13:49:57 -0400

    Latest updates

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,37 @@
 /* eslint-disable no-undef */
-import svelte from "rollup-plugin-svelte";
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import livereload from "rollup-plugin-livereload";
-import { terser } from "rollup-plugin-terser";
-import css from "rollup-plugin-css-only";
-import sveltePreprocess from "svelte-preprocess";
-import scss from "rollup-plugin-scss";
-import { config } from "dotenv";
-import replace from "@rollup/plugin-replace";
+import svelte from "rollup-plugin-svelte"
+import commonjs from "@rollup/plugin-commonjs"
+import resolve from "@rollup/plugin-node-resolve"
+import livereload from "rollup-plugin-livereload"
+import { terser } from "rollup-plugin-terser"
+import css from "rollup-plugin-css-only"
+import sveltePreprocess from "svelte-preprocess"
+import scss from "rollup-plugin-scss"
+import { config } from "dotenv"
+import replace from "@rollup/plugin-replace"
 import postcss from "rollup-plugin-postcss"
 
-const production = !process.env.ROLLUP_WATCH;
+const production = !process.env.ROLLUP_WATCH
 
 function serve() {
-	let server;
+	let server
 
 	function toExit() {
-		if (server) server.kill(0);
+		if (server) server.kill(0)
 	}
 
 	return {
 		writeBundle() {
-			if (server) return;
+			if (server) return
 			server = require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
 				stdio: ["ignore", "inherit", "inherit"],
 				shell: true
-			});
+			})
 
-			process.on("SIGTERM", toExit);
-			process.on("exit", toExit);
+			process.on("SIGTERM", toExit)
+			process.on("exit", toExit)
 		}
-	};
+	}
 }
 
 
@@ -100,4 +100,4 @@ export default {
 	watch: {
 		clearScreen: false
 	}
-};
+}
