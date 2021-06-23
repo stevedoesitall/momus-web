002099425b3f0af2754ca799b4d9988331a90564
Steve Giordano <stephenagiordano@gmail.com>
2021-06-22 23:52:01 -0400

    Tweaked the nav bar a bit; working on a hamburger for mobile

--- src/components/UI/Nav.svelte
+++ src/components/UI/Nav.svelte
@@ -1,9 +1,31 @@
-<nav class="navbar is-primary" role="navigation" aria-label="main navigation">
-	<div id="navbarBasicExample" class="navbar-menu">
+<nav
+	class="navbar is-primary is-desktop-only"
+	role="navigation"
+	aria-label="main navigation"
+>
+	<div class="navbar-brand">
+		<a class="navbar-item" href="/"> Home </a>
+
+		<a class="navbar-item" href="/about"> About </a>
+		<!-- <a
+			role="button"
+			class="navbar-burger"
+			aria-label="menu"
+			aria-expanded="false"
+		>
+			<span aria-hidden="true" />
+			<span aria-hidden="true" />
+			<span aria-hidden="true" />
+		</a> -->
+		<span aria-hidden="true" />
+		<span aria-hidden="true" />
+		<span aria-hidden="true" />
+	</div>
+	<!-- <div class="navbar-menu is-hidden-mobile ">
 		<div class="navbar-start">
 			<a class="navbar-item" href="/"> Home </a>
 
 			<a class="navbar-item" href="/about"> About </a>
 		</div>
-	</div>
+	</div> -->
 </nav>
